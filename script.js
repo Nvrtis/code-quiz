5925039fa93a62ca5890fd082f90bab14cb7ecc3
Nicholas Vrtis <nvav91@gmail.com>
2020-12-02 18:03:49 +0100

    psedocoding

---
+++ script.js
@@ -0,0 +1,47 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+
+startButton.addEventListener('click', startGame)
+
+
+function startGame() {
+
+}
+
+function setNextQuestions() {
+
+}
+
+function selectAnswer() {
+
+}
+
+
+
+
+// quiz
+// have a intro menu in html where a button starts the quiz
+// quiz should change the menu with the quiz with a question and 4 buttons that should have the answer on it. only 1 is correct
+// quiz should also start with a timer that is displayed for the user
+// when the correct button is clicked is should add 1 point to the users score.
+// when the incorrect button is clicked the timer should be reduced a certain amount
+// when timer reaches 0  the quiz will be over
+// when quiz is over, then the user can save its initials and score to local
+// after the user have save the score, the quiz then restart at the menu site
+
+
+
+// Acceptance Criteria
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+// Mock-Up
+// The following animation demonstrates the application functionality:
